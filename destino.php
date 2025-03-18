66ecff5d2ef86cb3c566092737b2774b56720fca
zModz <bbenfica99@gmail.com>
2025-03-18 14:12:47 +0000

    done?

---
+++ destino.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link
+        rel="shortcut icon"
+        href="./imgs/logos/mods_logo.jpg"
+        type="image/x-icon" />
+    <!-- Bootstrap Icons -->
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <title>MODS - Success</title>
+</head>
+
+<body>
+    <div id="nav">
+        <div id="hamMenu">
+            <i class="bi bi-list"></i>
+        </div>
+        <h1 id="logo">MODS</h1>
+        <div id="menu">
+            <a href="index.html">Home</a>
+            <a href="projects.html">Projects</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </div>
+        <div id="hireBtn" class="btn">Hire me<i class="bi bi-send"></i></div>
+    </div>
+
+    <main id="successMain">
+        <div id="success" class="cxMain">
+            <i class="bi bi-check-circle"></i>
+            <h1>Success!</h1>
+            <p>Your message has been sent successfully!</p>
+            <p>I will get back to you as soon as possible.</p>
+        </div>
+    </main>
+
+    <footer>
+        <p>Made with ❤️ from Portugal</p>
+        <span> © 2025 Leonardo Ramos </span>
+    </footer>
+</body>
+
+</html>
