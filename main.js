885d13f9f3d6b436fcd347e7eba511c382e35654
zModz <bbenfica99@gmail.com>
2025-05-25 18:17:35 +0100

    prep work for publication

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// drawer functionality
+document.getElementById("openDrawer").addEventListener("click", function () {
+  document.getElementById("drawer").style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+});
+document.getElementById("closeDrawer").addEventListener("click", function () {
+  document.getElementById("drawer").style.display = "none";
+  document.getElementsByTagName("body")[0].style.overflow = "auto";
+});
+
+// modal functionality
 document.getElementById("openModal").addEventListener("click", function () {
   document.getElementById("modal").style.display = "block";
   document.getElementsByTagName("body")[0].style.overflow = "hidden";
